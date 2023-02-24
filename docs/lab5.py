401e7acb4db5b9d87e4b2cdc4ea222a27e699db3
Hushmist <nurtai.0624@gmail.com>
2023-02-24 23:49:38 +0600

    Solved: lab5

--- docs/lab5.py
+++ docs/lab5.py
@@ -1,7 +1,8 @@
-def task5():
+import random
+
+def task1():
     students = list()
     students = []
-    sections = []
 
     students.append(['Alfa', "Section1"])
     students.append(['Beta', "Section2"])
@@ -16,4 +17,50 @@ def task5():
                     print(students.pop(i))
             except:
                 print()
-task5()
+
+def task2():
+    students = []
+    students.append(['Alfa', 5])
+    students.append(['Beta', 3])
+    students.append(['Gamma', 4])
+    students.append(['Delta', 5])
+
+    choosen = input('Enter student name')
+    for i in students:
+        if(i[0] == choosen):
+            print(i[1])
+
+def task3_4(is_desc = False):
+    numbers = []
+    inputed = 1
+    
+    while(1):
+        inputed = int(input("enter number(0 - stop)"))
+        if(inputed == 0):
+            break
+        numbers.append(inputed)
+    if(is_desc):
+        numbers.sort(reverse=True)
+    else:
+        numbers.sort()
+    for i in numbers:
+        print(i)
+
+def task5():
+    numbers = []
+    for i in range(1, 50):
+        numbers.append(i)
+    res = []
+    for i in range(6):
+        res.append(numbers.pop((random.randint(a=0,b=len(numbers)))))
+    return res
+
+def is_sorted(numbers):
+    return numbers == sorted(numbers)
+
+# task1()
+# task2()
+# task3_4()
+# task3_4(True)
+# print(task5())
+# print(is_sorted([1, 2]))
