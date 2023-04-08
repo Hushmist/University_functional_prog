2ab77cfa61b34e6b62c328f8e994c9202dba4e56
Jaksylyk1881 <jaksjuasbai@gmail.com>
2023-04-08 11:53:45 +0600

    Modify: lab 10

--- lab10.py
+++ lab10.py
@@ -1,20 +1,17 @@
 # 1 task
-def task1():
-    list1 = [88, 12, 15, 20, 25]
-    list2 = [2, 4, 6, 8, 10]
-
-    if all(x > max(list2) for x in list1):
-        pairs = list(zip(list1, reversed(list2)))
-        print(list(enumerate(pairs)))
-        averages = []
-        for i, pair in enumerate(pairs):
-            average = sum(pair) / 2
-            averages.append(round(average, 2))
-        print("Average", averages)
-    else:
-        print("Не все элементы первого массива больше чем второй массив")
-task1()
-# print(example1([10, 5]))
+def task1(max_list, min_list):
+    check_list = []
+    for x in max_list:
+        check_list.append(x > max(min_list))
+    if(not all(check_list)):
+        return None
+    pairs = list(zip(max_list, reversed(min_list)))
+    averages = []
+    for i, pair in enumerate(pairs):
+        average = sum(pair) / 2
+        averages.append(round(average, 2))
+    return (averages)
+print(task1([55, 2, 33], [11, 13, -8]))
 
 # 2.1 task
 #  Напишите программу, которая считывает строку с клавиатуры и 
@@ -42,41 +39,46 @@ def _all(some_string):
 # 2.3 Реализуйте функцию, которая принимает матрицу (список списков) и 
 # возвращает матрицу, повернутую на 90 градусов по часовой стрелке. 
 # Используйте функции zip(), list() и reversed()(
-def _rotate(matrix):
-    _reversed = reversed(matrix)
-    zipped = list(zip(_reversed))
+def _rotate_(matrix):
+    _reversed = list(reversed(matrix))
+    zipped = (list(zip(_reversed[0], _reversed[1], _reversed[2])))
     return zipped
 
-# print(_rotate([[2,5],
-#                [7,9]]))
+print(_rotate_([[2,5,5],
+               [7,9,6],
+               [5,7,3]]))
 
-def knapsack(weights, costs, max_weight):
-    n = len(weights)
-    dp = [[0] * (max_weight + 1) for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        for j in range(1, max_weight + 1):
-            if weights[i - 1] <= j:
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + costs[i - 1])
-            else:
-                dp[i][j] = dp[i - 1][j]
-    return dp[n][max_weight]
+# 2.4Реализуйте функцию для решения задачи о рюкзаке с помощью 
+# динамического программирования. Функция принимает два списка 
+# (веса и стоимости предметов) и максимальный вес рюкзака, а 
+# возвращает максимальную стоимость, которую можно унести в 
+# рюкзаке. Используйте функции range(), enumerate() и max()
 
-# weights = [10, 20, 30]
-# costs = [60, 100, 120]
-# max_weight = 50
-# print(knapsack(weights, costs, max_weight))
 
+# Реализуйте функцию, которая выполняет операции над двумя 
+# матрицами (сложение, вычитание, умножение). Функция принимает 
+# две матрицы и символ операции, а возвращает результат операции. 
+# Используйте функции enumerate(), zip() и len()
 def matrix_operation(matrix1, matrix2, operation):
-    if operation == '+':
-        return [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
-    elif operation == '-':
-        return [[matrix1[i][j] - matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
-    elif operation == '*':
-        return [[sum([matrix1[i][k] * matrix2[k][j] for k in range(len(matrix1[0]))]) for j in range(len(matrix2[0]))] for i in range(len(matrix1))]
-    else:
-        return None
+    _list = []
+    match operation:
+        case '+':
+            for i in range(len(matrix1)):
+                for j in range(len(matrix1[0])):
+                    _list.append(matrix1[i][j] + matrix2[i][j])
+            return _list
+        case '-':
+            for i in range(len(matrix1)):
+                for j in range(len(matrix1[0])):
+                    _list.append(matrix1[i][j] - matrix2[i][j])
+            return _list
+        case '*':
+            _new_list = []
+            for _, i in enumerate(range(len(matrix1))):
+                for j in range(len(matrix2[0])):
+                    _list.append([sum([matrix1[i][k] * matrix2[k][j] for k in range(len(matrix1[0]))])])
+            for i in range(0, len(_list), 2):
+                _new_list.append(list(zip(_list[i], _list[i+1])))
+            return _new_list
     
-# matrix1 = [[1, 2], [3, 4]]
-# matrix2 = [[5, 6], [7, 8]]
-# operation = '+'
-# print(matrix_operation(matrix1, matrix2, operation))
+print(matrix_operation([[1, 2], [3, 4]], [[5, 6], [7, 8]], '*'))
